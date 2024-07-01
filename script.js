6c3ed97883b544af7faa6295ed9522743cde8644
hom3mad3magick <contact@hom3mad3magick.com>
2024-07-01 13:30:41 -0700

    Update script.js
    updated script js

--- script.js
+++ script.js
@@ -21,10 +21,12 @@ document.getElementById('personality-test').addEventListener('submit', function(
   resultsContainer.style.borderRadius = '10px';
   resultsContainer.innerText = results;
 
-  document.body.appendChild(resultsContainer);
+  // Append the results container to the form
+  event.target.appendChild(resultsContainer);
 
   // Redirect to homepage after displaying results
   setTimeout(() => {
     window.location.href = 'https://www.hom3mad3magick.com';
   }, 5000); // 5 seconds delay
 });
+
